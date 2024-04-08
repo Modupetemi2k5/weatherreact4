d6a11b6313f2748b3d1887ba9b809eca2a91d5c1
Modupetemi2k5 <151980601+Modupetemi2k5@users.noreply.github.com>
2024-04-08 16:21:46 -0700

    react weartherapp

---
+++ src/Weather.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+
+export default function Weather(props) {
+    let [city, setCity] = useState("");
+    let [temperature, setTemperature] = useState("");
+    let [description, setDesccription] = useState("");
+    let [wind, setWind] = useState(" ");
+    let [humidity, setHumidity] = useState(" ");
+    let { icon, setIcon } = useState(" ");
+
+    function showWeather(response) {
+        console.log(response.data);
+        setTemperature(response.data.condition.temperature)
+        setDesccription(response.data.condition.description)
+        setHumidity(response.data.temperature.humidity)
+        setWind(response.data.speed.wind)
+        setIcon( < img src = {
+                response.data.icon_url
+            }
+            /> )
+        }
+
+        function handleSubmit(event) {
+            let apiKey = "fbef01f4et1b02o0d25c27210a43ef3f";
+            let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+            event.preventDefault();
+            axios.get(apiUrl).then(showWeather);
+        }
+
+        function searchCity(event) {
+            event.preventDefault();
+            setCity(event.target.value;)
+        }
+
+        return ( < div className = "my=form" > { " " } < form onChange = { handleSubmit } > { " " } < input type = "search"
+            placeholder = "enter-city"
+            value = { city }
+            />{" "} <input type = "submit" value = { search } onSubmit = { searchCity } / >
+            (" ") < /form > { " " } < /div >
+
+            {
+                temperature && ( < div > < h1 > Temperature: { temperature } < /h1 ></div > )
+            }
+
+
+            {
+                humidity &&
+                    ( < div > < h1 > Humidity: { humidity } < /h1 ></div > )
+            }
+
+
+            {
+                description && ( < div > < h1 > Description: { description } < /h1 ></div > )
+            }
+
+
+            {
+                wind &&
+                    ( < div > < h1 > Wind: { wind } < /h1 ></div > )
+            }
+
+
+            {
+                icon && ( < div > < h1 > Icon: { icon } < /h1 ></div > )
+            }
+
+        )
+    }
