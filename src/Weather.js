4273e8a444371fc95092af8b5cd5ac7864bf6ecc
Modupetemi2k5 <151980601+Modupetemi2k5@users.noreply.github.com>
2024-04-09 12:31:16 -0700

    adding atl to icon

--- src/Weather.js
+++ src/Weather.js
@@ -16,55 +16,58 @@ export default function Weather(props) {
         setDesccription(response.data.condition.description)
         setHumidity(response.data.temperature.humidity)
         setWind(response.data.speed.wind)
-        setIcon( < img src = {
+        setIcon( < img alt = "icon"
+            src = {
                 response.data.icon_url
             }
-            /> )
-        }
-
-        function handleSubmit(event) {
-            let apiKey = "fbef01f4et1b02o0d25c27210a43ef3f";
-            let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-            event.preventDefault();
-            axios.get(apiUrl).then(showWeather);
-        }
-
-        function searchCity(event) {
-            event.preventDefault();
-            setCity(event.target.value;)
-        }
-
-        return ( < div className = "my=form" > { " " } < form onChange = { handleSubmit } > { " " } < input type = "search"
-            placeholder = "enter-city"
-            value = { city }
-            />{" "} <input type = "submit" value = { search } onSubmit = { searchCity } / >
-            (" ") < /form > { " " } < /div >
-
-            {
-                temperature && ( < div > < h1 > Temperature: { temperature } < /h1 ></div > )
-            }
-
-
-            {
-                humidity &&
-                    ( < div > < h1 > Humidity: { humidity } < /h1 ></div > )
-            }
-
-
-            {
-                description && ( < div > < h1 > Description: { description } < /h1 ></div > )
-            }
-
-
-            {
-                wind &&
-                    ( < div > < h1 > Wind: { wind } < /h1 ></div > )
-            }
-
-
-            {
-                icon && ( < div > < h1 > Icon: { icon } < /h1 ></div > )
-            }
-
+            />
         )
     }
+
+    function handleSubmit(event) {
+
+        let apiKey = "fbef01f4et1b02o0d25c27210a43ef3f";
+        let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+        event.preventDefault();
+        axios.get(apiUrl).then(showWeather);
+    }
+
+    function searchCity(event) {
+        event.preventDefault();
+        setCity(event.target.value)
+    }
+
+    return ( < div className = "my-form" > { " " } < form onChange = { handleSubmit } > { " " } < input type = "search"
+        placeholder = "enter-city"
+        value = { city }
+        />{" "} <input type = "submit" value = { search } onSubmit = { searchCity } / > (" ") < /form > { " " } < /div >
+
+        {
+            temperature &&
+            ( < div > < h1 > Temperature: { temperature } < /h1 ></div > )
+        }
+
+
+        {
+            humidity &&
+                ( < div > < h1 > Humidity: { humidity } < /h1 ></div > )
+        }
+
+
+        {
+            description && ( < div > < h1 > Description: { description } < /h1 ></div > )
+        }
+
+
+        {
+            wind &&
+                ( < div > < h1 > Wind: { wind } < /h1 ></div > )
+        }
+
+
+        {
+            icon && ( < div > < h1 > Icon: { icon } < /h1 ></div > )
+        }
+
+    )
+}
