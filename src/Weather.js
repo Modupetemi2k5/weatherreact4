8c0cb8f77217814285c4d0eafb5bd9d7ab50f198
Modupetemi2k5 <151980601+Modupetemi2k5@users.noreply.github.com>
2024-04-09 16:27:48 -0700

    adding

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-
 export default function Weather(props) {
     let [city, setCity] = useState("");
     let [temperature, setTemperature] = useState("");
@@ -12,63 +11,79 @@ export default function Weather(props) {
 
     function showWeather(response) {
         console.log(response.data);
-        setTemperature(response.data.condition.temperature)
-        setDesccription(response.data.condition.description)
-        setHumidity(response.data.temperature.humidity)
-        setWind(response.data.speed.wind)
+        setTemperature(response.data.condition.temperature);
+        setDesccription(response.data.condition.description);
+        setHumidity(response.data.temperature.humidity);
+        setWind(response.data.speed.wind);
         setIcon( < img alt = "icon"
-            src = {
-                response.data.icon_url
+            src = { response.data.icon_url }
+            />);
+        }
+
+        function handleSubmit(event) {
+            let apiKey = "fbef01f4et1b02o0d25c27210a43ef3f";
+            let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+            event.preventDefault();
+            axios.get(apiUrl).then(showWeather);
+        }
+
+        function searchCity(event) {
+            event.preventDefault();
+            setCity(event.target.value);
+        }
+
+        return ( < div className = "returm" >
+            <
+            div className = "my-forrm" > { " " } < form onSubmit = { handleSubmit } > { " " } <
+            input type = "search "
+            placeholder = "enter-city"
+            value = { city }
+            onChange = { searchCity }
+            />{" "} <
+            input type = "submit" / >
+            (" ") { " " } < /form > { " " } < /div > { " " } {
+
+                temperature && ( < div > { " " } < h1 > Temperature: { temperature } < /h1>{" "} < /
+                    div >
+                )
+            } { " " } {
+                humidity && ( <
+                    div > { " " } <
+                    h1 > Humidity: { humidity } < /h1>{" "} < /
+                    div >
+                )
             }
-            />
-        )
-    }
 
-    function handleSubmit(event) {
+            { " " }
 
-        let apiKey = "fbef01f4et1b02o0d25c27210a43ef3f";
-        let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-        event.preventDefault();
-        axios.get(apiUrl).then(showWeather);
-    }
+            {
+                description && ( < div > { " " } < h1 > Description: { description } <
+                    /h1>{" "} <
+                    div >
+                )
+            }
 
-    function searchCity(event) {
-        event.preventDefault();
-        setCity(event.target.value)
-    }
+            { " " }
 
-    return ( < div className = "my-form" > { " " } < form onChange = { handleSubmit } > { " " } < input type = "search"
-        placeholder = "enter-city"
-        value = { city }
-        onSubmit = { searchCity }
-        />{" "} <input type = "submit"  / > (" ") < /form > { " " } < /div >
+            {
+                wind && ( <
+                    div > { " " } <
+                    h1 > Wind: { wind } <
+                    /h1>{" "} 
 
-        {
-            temperature &&
-            ( < div > < h1 > Temperature: { temperature } < /h1 ></div > )
-        }
+                    <
+                    /
+                    div >
+                )
+            }
 
+            { " " }
 
-        {
-            humidity &&
-                ( < div > < h1 > Humidity: { humidity } < /h1 ></div > )
-        }
-
-
-        {
-            description && ( < div > < h1 > Description: { description } < /h1 ></div > )
-        }
-
-
-        {
-            wind &&
-                ( < div > < h1 > Wind: { wind } < /h1 ></div > )
-        }
-
-
-        {
-            icon && ( < div > < h1 > Icon: { icon } < /h1 ></div > )
-        }
-
-    )
-}
+            {
+                icon && ( < div > { " " } <
+                    h1 > Icon: { icon } <
+                    /h1>{" "} < /
+                    div >
+                )
+            } { " " } < div / >
+        );
