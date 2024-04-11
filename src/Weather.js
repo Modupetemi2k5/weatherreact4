eb0a4c683529f4a64b07cce6ef8fab404d03d6f4
Modupetemi2k5 <151980601+Modupetemi2k5@users.noreply.github.com>
2024-04-10 21:58:10 -0700

    add up

--- src/Weather.js
+++ src/Weather.js
@@ -11,12 +11,12 @@ export default function Weather(props) {
 
     function showWeather(response) {
         console.log(response.data);
-        setTemperature(response.data.condition.temperature);
+        setTemperature(response.data.temperature.current);
         setDesccription(response.data.condition.description);
         setHumidity(response.data.temperature.humidity);
-        setWind(response.data.speed.wind);
+        setWind(response.data.wind.speed);
         setIcon( < img alt = "icon"
-            src = { response.data.icon_url }
+            src = { response.data.condition.icon_url }
             />);
         }
 
@@ -32,23 +32,48 @@ export default function Weather(props) {
             setCity(event.target.value);
         }
 
-        return ( < div className = "returm" > { " " } < div className = "my-forrm" > { " " } < form onSubmit = { handleSubmit } > { " " } < input type = "search "
+        return ( <
+            > { " " } <
+            div className = "my-forrm" > { " " } <
+            form onSubmit = { handleSubmit } > { " " } <
+            input type = "search "
             placeholder = "enter-city"
             value = { city }
             onChange = { searchCity }
-            />{" "} <input type="submit"></input > (" ") { " " } < /form > { " " } < div > { " " } {
-
-
-
-            temperature && ( < div > { " " } < h1 > Temperature: { temperature } < /h1>{" "} </div > )
-        } { " " } {
-            humidity && ( < div > { " " } < h1 > Humidity: { humidity } < /h1>{" "} </div > )
-        } { " " } {
-            description && ( < div > { " " } < h1 > Description: { description } < /h1>{" "} </div > )
-        } { " " } {
-            wind && ( < div > { " " } < h1 > Wind: { wind } < /h1>{" "} </div > )
-        } { " " } {
-            icon && ( < div > { " " } < h1 > Icon: { icon } < /h1>{" "} </div > )
-        } { " " } < /div >
-    );
-}
+            />{" "} <
+            input type = "submit" / > (" ") { " " } <
+            /form>{" "} <
+            /div>{" "} {
+                temperature && ( <
+                    div > { " " } <
+                    h1 > Temperature: { temperature } < /h1>{" "} <
+                    /div>
+                )
+            } { " " } {
+                humidity && ( <
+                    div > { " " } <
+                    h1 > Humidity: { humidity } < /h1>{" "} <
+                    /div>
+                )
+            } { " " } {
+                description && ( <
+                    div > { " " } <
+                    h1 > Description: { description } < /h1>{" "} <
+                    /div>
+                )
+            } { " " } {
+                wind && ( <
+                    div > { " " } <
+                    h1 > Wind: { wind } < /h1>{" "} <
+                    /div>
+                )
+            } { " " } {
+                icon && ( <
+                    div > { " " } <
+                    h1 > Icon: { icon } < /h1>{" "} <
+                    /div>
+                )
+            } { " " } <
+            />
+        );
+    }
